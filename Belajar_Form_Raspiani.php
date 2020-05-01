515219cc586ffa9ab9ebb84d1735b49b77101413
@Raspiani_ <34181096+Raspiani@users.noreply.github.com>
2020-05-01 21:13:23 +0700

    Add files via upload

---
+++ Belajar_Form_Raspiani.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar Menggunakan Form Raspiani</title>
+</head>
+<body>
+<p>Belajar Menggunakan Form</p>
+<form action="file_tujuan.php" method="get">
+	Nama: <input type="text" name="Nama" />
+	<br>
+	<br>
+	Email: <input type="text" name="Email" />
+	<br><br>
+	<input type="submit" value="Kirim" />
+</form>
+</body>
+</html>
---
+++ Belajar_PHP_Raspiani.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Belajar HTML PHP MYSQL Bersama Raspiani </title>
+</head>
+<body style="background-color: aqua; color: black">
+<p>
+		<h1 align="center"><b> Belajar Yuk </b></h1>
+		<h2> 1. Belajar HTML </h2>
+		<p> 1.1 Struktur Dasar HTML terdiri dari:</p>
+		<ul style="list-style-type: square">
+			<li> Tag DTD atau DOCTYPE </li>
+			<p> DTD adalah kepanjangan dari <b><i>Document Type Declaration</i></b> </p>
+			
+			<li> Tag HTML </li>
+			<li> Tag Head </li>
+			<li> Tag Body </li>
+		</ul>
+		<h2> 2. RoadMap Data Scientist</h3>
+		<p>Berikut adalah RoadMap Singkat Menjadi Data Scientist</p>
+		<br>
+		<img src="Data Scientist.JPG" alt="RoadMap" width="100" height="100" style="float: left; margin-right: 20px">
+
+		<p> Mengenal data scientist dapat kita mulai dengan perkenalan data dan data scientist, yang kemudian mengenal matematika, statistika, python, advanced statistik, machine learning dan menuju data scientist </p>
+		<br>
+		<br>
+		
+
+		<h2>3. Belajar PHP </h2>
+
+
+		<p>
+			<?php
+		echo "Belajar HTML PHP MYSQL Bersama Raspiani ";
+		echo "Ayo Kita Belajar";
+
+		?>
+
+		</p>
+		
+
+	
+
+
+<hr>
+
+<p> Copyright &copy; Raspiani &trade; </p>
+
+</body>
+</html>
